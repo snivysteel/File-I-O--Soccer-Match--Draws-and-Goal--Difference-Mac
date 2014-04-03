9935177225286b5aea1d23dbe7d285669577644b
Ryan Wong <snivysteelhci@gmail.com>
2014-04-03 21:28:33 +0800

    Initial Commit

---
+++ O: Soccer Match (Draws and Goal Difference)/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  O: Soccer Match (Draws and Goal Difference)
+//
+//  Created by Ryan Wong on 3/4/14.
+//  Copyright (c) 2014 rwjhwong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
